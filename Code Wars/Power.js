1c1624f68e5de9e7c3d7ee90b3459024cbd507d5
James Miles <jimdgmiles@gmail.com>
2017-08-06 15:17:57 +0100

    Added Code Wars section.

---
+++ Code Wars/Power.js
@@ -0,0 +1,7 @@
+function numberToPower(number, power){
+  var answer=1;
+  for(var i=0; i<power; i++){answer=answer*number}
+  return answer;
+}
+
+// https://www.codewars.com/kata/power/train/javascript
